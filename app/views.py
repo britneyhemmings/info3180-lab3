c297882561377053dfe91c2287fc9e6365282ba3
Britney Hemmings <britneyhemmings1@gmail.com>
2023-02-18 21:01:21 -0500

    Edited starter files

--- app/views.py
+++ app/views.py
@@ -7,7 +7,9 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+from .forms import ContactForm
+from app import mail
+from flask_mail import Message
 
 ###
 # Routing for your application.
@@ -23,6 +25,29 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/contact', methods=['GET','POST'])
+def contact():
+    form = ContactForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        subject = form.subject.data
+        message = form.message.data
+
+        msg = Message(subject,
+        sender=(name, email),
+        recipients=["mjane@gmail.com"])
+        msg.body = message
+        mail.send(msg)
+
+        flash('Your email was successfully sent', 'success')
+        return redirect(url_for('home'))
+
+
+    flash_errors(form)
+    return render_template('contact.html', form = form)
 
 
 ###
